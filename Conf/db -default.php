36551966e25c128a78991830d71361bbc01c2c71
lwj-wuwang <lwj_wuwang@sina.com>
2016-04-18 15:17:50 +0800

    添加过滤文件

---
+++ Conf/db -default.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'weimixqq160415',
+  'DB_USER' => 'root',
+  'DB_PWD' => 'asd123321',
+  'DB_PREFIX' => 'tp_',
+  'agent_version' => 1,
+);
+?>
